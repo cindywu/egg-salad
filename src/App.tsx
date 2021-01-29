53f60f838ba1dd815e7e122407d928309d341499
cindy <ciindy.wu@gmail.com>
2021-01-28 18:39:27 -1000

    Make context work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, createContext, useContext } from 'react'
 import CounterHooks from './CounterHooks'
 
+const defaultTheme = 'red'
+
+type ThemeContextType = {
+  theme: string
+  setTheme: (value: string) => void
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+)
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const ThemeProvider = ({ children }: Props) => {
+  const [theme, setTheme] = useState(defaultTheme)
+
+  useEffect(() => {
+    setTheme(defaultTheme)
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)
+
+const Toggle = () => {
+  const { theme, setTheme } = useTheme ()!
+  console.log('toggle theme', theme)
+  return (
+    <button onClick={() => setTheme(theme === 'red' ? 'blue' : 'red')}>
+      Toggle Theme
+    </button> 
+  )
+}
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       CounterHooks
       <CounterHooks initialCount={0} />
-    </>
+      <Toggle />
+    </ThemeProvider>
   )
 }
 
--- src/CounterHooks.tsx
+++ src/CounterHooks.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, CSSProperties } from 'react'
+import { ThemeContext } from './App'
 
 interface CounterHooksProps {
   initialCount: number
@@ -6,9 +7,17 @@ interface CounterHooksProps {
 
 export default function CounterHooks({ initialCount }: CounterHooksProps) {
   const [count, setCount] = useState(initialCount)
+  const theme = useContext(ThemeContext)
+  
+  const color = theme?.theme
+
+  const style: CSSProperties = {
+    backgroundColor: color,
+  }
+  
   return (
     <div>
-      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button style={style} onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
       <span>{count}</span>
       <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
     </div>
